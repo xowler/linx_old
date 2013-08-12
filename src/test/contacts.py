e41ce02d949552cac96f1af9252213dd872d6845
Camilo <camilo.jimenezc+devel@gmail.com>
2013-08-12 15:16:58 -0400

    calculate cts in py

---
+++ src/test/contacts.py
@@ -0,0 +1,30 @@
+import linx
+
+gro = linx.gro('bhp.gro')
+h = [i for i,j in enumerate(gro[0]) if j['name'][0]!='H']
+rs = []
+lr = ""
+for i,a in enumerate(gro[0]):
+    if i in h:
+        if a['resname']!=lr: rs.append([]) 
+        rs[-1].append(i)
+        lr = a['resname']
+
+from numpy.linalg import norm
+def md(x1,x2):
+    m = 9999
+    for i in x1:
+        for j in x2:
+            m = min(m,norm(i-j))
+
+    return m
+
+x = gro[1]
+ds = []
+N = len(rs)
+for i in range(N):
+    for j in range(i+2,N):
+        ds.append(md(x[rs[i]], x[rs[j]]))
+
+        
+savetxt('cts.txt',ds)
